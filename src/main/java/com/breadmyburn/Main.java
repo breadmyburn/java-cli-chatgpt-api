7b8fbabb589dca256915ef269e3a7ef057b701d1
breadmyburn <18ang.mss@gmail.com>
2023-03-20 17:44:38 +0800

    feat: add continuous query

--- src/main/java/com/breadmyburn/Main.java
+++ src/main/java/com/breadmyburn/Main.java
@@ -17,42 +17,48 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Dotenv dotenv = Dotenv.load();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a prompt: ");
-        String prompt = scanner.nextLine();
+        while (true) {
+            System.out.println("Enter a query (or /e to exit): ");
+            String queryString = scanner.nextLine();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        ChatGptMessages chatGptMessages = new ChatGptMessages("user", prompt);
-        String messages = objectMapper.writeValueAsString((chatGptMessages));
-        ChatGptRequest chatGptRequest = new ChatGptRequest( "gpt-3.5-turbo", "[" + messages + "]");
-        String input = objectMapper.writeValueAsString(chatGptRequest);
+            if (queryString.equals("/e")) {
+                System.out.println("Goodbye.");
+                break;
+            }
 
-        /*
-        The output of the string `input` contains several "\" and two quotation marks in the
-        message portion of `ChatGptRequest` which makes the input not formatted correctly.
+            ObjectMapper objectMapper = new ObjectMapper();
+            ChatGptMessages chatGptMessages = new ChatGptMessages("user", queryString);
+            String messages = objectMapper.writeValueAsString((chatGptMessages));
+            ChatGptRequest chatGptRequest = new ChatGptRequest( "gpt-3.5-turbo", "[" + messages + "]");
+            String input = objectMapper.writeValueAsString(chatGptRequest);
 
-        The process below fixes the input and removes these additional characters.
-        */
-        String firstFix = input.replace("\\","");
-        String secondFix = firstFix.replace("\"[","[");
-        String inputFixed = secondFix.replace("]\"","]");
+            /*
+            The output of the string `input` contains several "\" and two quotation marks in the
+            message portion of `ChatGptRequest` which makes the input not formatted correctly.
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://chatgpt-api.shn.hk/v1/"))
-                .header("Content-Type", "application/json")
-                .header("Authorization", "Bearer " + dotenv.get("OPENAI_API_KEY"))
-                .POST(HttpRequest.BodyPublishers.ofString(inputFixed))
-                .build();
+            The process below fixes the input and removes these additional characters.
+            */
+            String firstFix = input.replace("\\","");
+            String secondFix = firstFix.replace("\"[","[");
+            String inputFixed = secondFix.replace("]\"","]");
 
-        HttpClient httpClient = HttpClient.newHttpClient();
-        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://chatgpt-api.shn.hk/v1/"))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + dotenv.get("OPENAI_API_KEY"))
+                    .POST(HttpRequest.BodyPublishers.ofString(inputFixed))
+                    .build();
 
-        if(response.statusCode() == 200) {
-            ChatGptResponse chatGptResponse = objectMapper.readValue(response.body(), ChatGptResponse.class);
-            System.out.println(chatGptResponse.getChoices()[0].message().content());
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
 
-        } else {
-            System.out.println(response.statusCode());
-            System.out.println(response.body());
+            if(response.statusCode() == 200) {
+                ChatGptResponse chatGptResponse = objectMapper.readValue(response.body(), ChatGptResponse.class);
+                System.out.println(chatGptResponse.getChoices()[0].message().content() + "\n");
+            } else {
+                System.out.println(response.statusCode());
+                System.out.println(response.body());
+            }
         }
     }
 }
