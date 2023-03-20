31171a88a00672cef872525ffc627c4f293a6a17
breadmyburn <18ang.mss@gmail.com>
2023-03-20 16:31:40 +0800

    feat: return choices from chatgpt api

--- src/main/java/com/breadmyburn/model/ChatGptResponse.java
+++ src/main/java/com/breadmyburn/model/ChatGptResponse.java
@@ -7,25 +7,9 @@ public record ChatGptResponse(
         String model,
         ChatGptResponseUsage usage,
         ChatGptResponseChoice[] choices
-        ) {
-}
+) {
 
-/*
-{
-    "id":"chatcmpl-6vpRjGyrGRsupI3YHxA7P0mN51JHu",
-    "object":"chat.completion",
-    "created":1679240475,
-    "model":"gpt-3.5-turbo-0301",
-    "usage":{"prompt_tokens":11,"completion_tokens":25,"total_tokens":36},
-    "choices":[
-        {
-            "message":{
-                "role":"assistant",
-                "content":"\n\nAs an AI language model, I don't have emotions, but I'm functioning well. How can I assist you?"
-        },
-        "finish_reason":"stop",
-        "index":0
-        }
-    ]
+    public ChatGptResponseChoice[] getChoices() {
+        return this.choices;
+    }
 }
-*/
